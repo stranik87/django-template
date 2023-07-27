f9ac95d26890751a80dab7f4bb40ad319cfcad43
stranik87 <tulkin.boynazarov@gmail.com>
2023-07-27 12:52:03 +0300

    grid style

--- api/views.py
+++ api/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 import requests
 
 
+
 def index(request):
     n = request.GET.get('n', 1)
     r = requests.get(f'https://randomuser.me/api/?results={n}')
     if r.status_code == 200:
         users = r.json()['results']
+        
     else:
-        user = None
+        users = None
     
     return render(request, 'index.html', {'users': users})
